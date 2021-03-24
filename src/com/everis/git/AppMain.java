cb98e1bcc551f598cac5b69e54f272cc32925505
scastrga <scastrga@everis.com>
2021-03-24 14:10:09 +0100

    Version 2 - GIT

--- src/com/everis/git/AppMain.java
+++ src/com/everis/git/AppMain.java
@@ -2,6 +2,11 @@ package com.everis.git;
 
 public class AppMain {
 
+	/**
+	 * Formacion GIT
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
